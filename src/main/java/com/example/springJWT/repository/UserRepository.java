13ad03651e0a84ba41ab0922d160022528130042
Lee haejun <teamforhj@gmail.com>
2025-01-05 14:02:46 +0900

    스프링 시큐리티 JWT 6 : 회원가입 로직 구현

--- src/main/java/com/example/springJWT/repository/UserRepository.java
+++ src/main/java/com/example/springJWT/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.springJWT.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springJWT.entity.UserEntity;
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    Boolean existsByUsername(String username);
 }
